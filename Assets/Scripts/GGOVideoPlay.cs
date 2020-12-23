3e50e98e84f9095cfd7da63f3cbe29d1d6032de2
Nishshanka Welagedara <dnsuriya@gmail.com>
2020-12-24 00:57:21 +1300

    First commit

---
+++ Assets/Scripts/GGOVideoPlay.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
+
+public class GGOVideoPlay : MonoBehaviour {
+    [SerializeField] VideoPlayer player = null;
+    [SerializeField] GameObject toBeEnabled = null;
+
+    private void OnEnable()
+    {
+        player.loopPointReached += EndPlayer;
+    }
+
+    private void OnDisable()
+    {
+        player.loopPointReached -= EndPlayer;
+    }
+
+    void EndPlayer(VideoPlayer p)
+    {
+        toBeEnabled.SetActive(true);
+        Destroy(gameObject);
+    }
+}
